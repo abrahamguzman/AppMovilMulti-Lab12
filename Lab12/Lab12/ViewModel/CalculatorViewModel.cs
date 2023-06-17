0748152d68b52e71c9e584574d8c007a8e0bf192
Abraham Guzman <85902876+abrahamguzman@users.noreply.github.com>
2023-06-16 20:05:25 -0500

    Primer commit

---
+++ Lab12/Lab12/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab12.ViewModel
+{
+    public class CalculatorViewModel :INotifyPropertyChanged
+    {
+        private string result;
+        public CalculatorViewModel()
+        {
+            CalculateCommand = new Command<string>(Calculate);
+            ClearCommand = new Command(Clear);
+            result = string.Empty;
+
+        }
+        public string Result
+        {
+            get { return result; }
+            set
+            {
+                if (result != value)
+                {
+                    result = value;
+                    OnPropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        public ICommand CalculateCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        private void Calculate(string parameter)
+        {
+            if (parameter == "=")
+            {
+                Result = new DataTable().Compute(Result, null).ToString();
+            }
+            else
+            {
+                Result += parameter;
+            }
+        }
+
+        private void Clear()
+        {
+            Result = string.Empty;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
